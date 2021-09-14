83955a971b03954ee3796e6f1beaabd1daa90ea7
João Gabriel Camargo de Freitas <83920066+JoaoFreitasGab@users.noreply.github.com>
2021-09-14 12:37:25 -0300

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Clone Netflix',
     description: "Neste projeto foi usado React e além disso foi usada uma API para manipulação de dados de filmes.",
-      image: '/images/1.png',
+      image: '/images/1.PNG',
       tags: ['React', 'API', 'HTML'],
     source: 'https://github.com/JoaoFreitasGab/react-netflix',
   
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Banco Digital',
     description:"Projeto de anuncio de um Banco 100% digital feito em totalmente React com smooth scroll. ",
-    image: '/images/3.png',
+    image: '/images/3.PNG',
     tags: ['React', 'JavaScript','HTML'],
     source: 'https://google.com',
    
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Pizzaria',
       description: "Projeto de uma pizzaria feito em React com cardápio integrado e com o uso de styled components. ",
-      image: '/images/2.png',
+      image: '/images/2.PNG',
       tags: ['React', 'HTML'],
     source: 'https://github.com/JoaoFreitasGab/react-pizza',
     
